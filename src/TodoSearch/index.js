5658ab924dceced8b4f3a3c45252bbdb9651d531
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-08-08 16:45:33 -0300

    Formulario para enviar ToDos,listo

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -13,7 +13,7 @@ function TodoSearch() {
   return (
     <input
       className="TodoSearch"
-      placeholder="Cebolla"
+      placeholder="Buscar Tarea"
       value={searchValue}
       onChange={onSearchValueChange}
     />
