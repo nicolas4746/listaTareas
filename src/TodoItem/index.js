1d6bde6344c35686f809af848547c0ba075597ca
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-08-09 11:06:03 -0300

    lololo

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -7,6 +7,8 @@ function TodoItem(props) {
       <span
         className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
         onClick={props.onComplete}
+
+        
       >
         √
       </span>
